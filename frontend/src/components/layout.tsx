78e1c20e3261626444c72ef99fca8350458198b6
Ana Clara R. Rodrigues da Cunha <anaclararrc@hotmail.com>
2024-06-12 15:00:34 -0300

    Styling inserir.tsx.

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -10,15 +10,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
         <>
-            <header className="w-full h-14 bg-blue-500 flex items-center">
+            <header className="w-full h-14 bg-blue-500 flex items-center shadow-lg">
                 <h1 className="text-white text-xl font-bold ml-3">Prótotipo</h1>
                 {mudouPagina
                     ? <button className="ml-auto mr-4" onClick={trocarBotao}>
-                        <Link to={'/'} className="bg-white p-2 rounded">Enviar Contrato</Link>
+                        <Link to={'/'} className="bg-white text-black-500 p-2 px-4 rounded-full shadow-md hover:bg-blue-100 transition-colors duration-300">Enviar Contrato</Link>
                     </button>
 
                     : <button className="ml-auto mr-4" onClick={trocarBotao}>
-                        <Link to={'/busca'} className="bg-white p-2 rounded">Ver Contratos</Link>
+                        <Link to={'/busca'} className="bg-white text-black-500 p-2 px-4 rounded-full shadow-md hover:bg-blue-100 transition-colors duration-300">Ver Contratos</Link>
                     </button>
                 }
             </header>
--- frontend/src/pages/inserir.tsx
+++ frontend/src/pages/inserir.tsx
@@ -1,13 +1,16 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import contratoDAO from "../DAOs/contratoDAO";
 
 export default function Inserir() {
+    const [fileName, setFileName] = useState("");
 
     async function submitArquivo(e: ChangeEvent<HTMLInputElement>) {
         const file = e.target.files![0]
         const formData = new FormData()
 
         formData.append("file", file)
+
+        setFileName(file.name)
 
         //FETCH API
         try {
@@ -35,10 +38,15 @@ export default function Inserir() {
 
     return (
         <>
-            <h2 className="font-bold text-3xl mt-3">Análise de contratos</h2>
+            <h2 className="font-bold text-3xl mt-4">Análise de contratos</h2>
             <p className="mt-2 text-xl">Visualize informações sobre seu contrato, de forma rápida e fácil</p>
 
-            <input type="file" accept=".pdf" onChange={submitArquivo} className="bg-[#1262FF] py-5 px-24 mt-8 text-white text-xl rounded" />
+            <label htmlFor="file-upload" className="bg-[#1262FF] font-bold py-4 px-10 mt-8 text-white text-xl rounded-md shadow-md hover:bg-blue-700 transition-colors duration-300 cursor-pointer">
+                Enviar Contrato
+            </label>
+            <input id="file-upload" type="file" accept=".pdf" onChange={submitArquivo} className="hidden"/>
+
+            {fileName && <p className="mt-4">Arquivo enviado: {fileName}</p>}
         </>
     )
 }
