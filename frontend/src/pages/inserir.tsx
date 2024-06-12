aea7844aab57c691bbe556a9f6425b323ca90f45
Ruan Emanuell Alencar <113607857+RuanEmanuell@users.noreply.github.com>
2024-06-12 16:29:25 -0300

    Adding switch for tests

--- frontend/src/pages/inserir.tsx
+++ frontend/src/pages/inserir.tsx
@@ -1,14 +1,17 @@
 import { ChangeEvent, useState } from "react";
 import contratoDAO from "../DAOs/contratoDAO";
+import CustomSwitch from "../components/switch";
 
 export default function Inserir() {
     const [fileName, setFileName] = useState("");
+    const [analysisMode, setAnalysisMode] = useState("default");
 
     async function submitArquivo(e: ChangeEvent<HTMLInputElement>) {
         const file = e.target.files![0]
         const formData = new FormData()
 
-        formData.append("file", file)
+        formData.append("file", file);
+        formData.append("mode", analysisMode);
 
         setFileName(file.name)
 
@@ -36,16 +39,22 @@ export default function Inserir() {
         }
     }
 
+    function switchAnalysisMode() {
+        setAnalysisMode(prev => prev == "default" ? "exact" : "default")
+    }
+
     return (
         <>
+            <div className="absolute right-4 my-4 w-max flex flex-col items-center justify-center">
+                <CustomSwitch mode={analysisMode} onClick={switchAnalysisMode} />
+                <p className="text-center my-2">Modo exato</p>
+            </div>
             <h2 className="font-bold text-3xl mt-4">Análise de contratos</h2>
             <p className="mt-2 text-xl">Visualize informações sobre seu contrato, de forma rápida e fácil</p>
-
             <label htmlFor="file-upload" className="bg-[#1262FF] font-bold py-4 px-10 mt-8 text-white text-xl rounded-md shadow-md hover:bg-blue-700 transition-colors duration-300 cursor-pointer">
                 Enviar Contrato
             </label>
-            <input id="file-upload" type="file" accept=".pdf" onChange={submitArquivo} className="hidden"/>
-
+            <input id="file-upload" type="file" accept=".pdf" onChange={submitArquivo} className="hidden" />
             {fileName && <p className="mt-4">Arquivo enviado: {fileName}</p>}
         </>
     )
