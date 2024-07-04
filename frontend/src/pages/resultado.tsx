03c9a9b9b6dbe58afb343e4b08a35eb05301c092
Ana Clara R. Rodrigues da Cunha <anaclararrc@hotmail.com>
2024-07-04 17:12:15 -0300

    (Version 1.0) Adjusting Design again

--- frontend/src/pages/resultado.tsx
+++ frontend/src/pages/resultado.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { CaretCircleLeft } from "@phosphor-icons/react";
 
 export default function Resultado() {
     const { state } = useLocation();
     const [maiorValor, setMaiorValor] = useState<number>(0)
     const [classificacao, setClassificacao] = useState<string>("")
     const result = state?.result; // Obtém o resultado do estado
+    const navigate = useNavigate();
 
     const [pdfUrl, setPdfUrl] = useState('');
 
@@ -56,25 +58,32 @@ export default function Resultado() {
 
     return (
         <>
+            <button
+                onClick={() => navigate("/busca")} // Navega para a página de busca
+                className="absolute top-24 left-4 flex items-center gap-2 bg-[#4510a3] h-11 px-4 text-white rounded-md"
+            >
+                <CaretCircleLeft size={23} weight="fill" />
+                <p>Página de Busca</p>
+            </button>
+
             <h1 className="text-2xl text-black font-bold mt-4">RESULTADO</h1>
             <div className="flex flex-row justify-center gap-2 items-center mt-3">
                 <h2 className="text-xl text-black">O Documento se classifica como: </h2>
                 <p className="text-xl text-[#4510a3] font-semibold">{classificacao}</p>
             </div>
 
-            <div className="flex gap-6 w-full">
-                <div className="bg-[#F9F9F9]  hover:bg-[#4510a3] hover:text-white flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md flex-1 gap-4 mt-8">
+            <div className="flex gap-6 w-full justify-center">
+                <div className="bg-[#F9F9F9] hover:bg-[#efeef4] flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md w-1/3 gap-4 mt-8">
                     <p className="text-xl">{`Nome do Arquivo: ${result.nomeArquivo}`}</p>
                     <p className="text-xl">{`CNPJs: ${result.contrato.cnpjs.join(', ')}`}</p>
                 </div>
-                <div className="bg-[#F9F9F9]  hover:bg-[#4510a3] hover:text-white flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md flex-1 gap-4 mt-8">
+                <div className="bg-[#F9F9F9] hover:bg-[#efeef4] flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md w-1/3 gap-4 mt-8">
                     <p className="text-xl">{`Contratante: ${result.contrato.contractant}`}</p>
                     <p className="text-xl">{`Contratado: ${result.contrato.contractor}`}</p>
                 </div>
             </div>
             <p className="text-xl mt-8">{`Data de vigência do contrato: ${result.contrato.date}`}</p>
 
-            
             {/*<iframe
                 src={pdfUrl}
                 width="100%"
