a7a9fdd8457539125d1170287e0594d26579088c
Diogo Gomes <diogogomescastro@outlook.com>
2024-07-05 00:48:24 -0300

    Add responsive (no end)

--- frontend/src/pages/resultado.tsx
+++ frontend/src/pages/resultado.tsx
@@ -60,29 +60,29 @@ export default function Resultado() {
         <>
             <button
                 onClick={() => navigate("/busca")} // Navega para a página de busca
-                className="absolute top-24 left-4 flex items-center gap-2 bg-[#4510a3] h-11 px-4 text-white rounded-md"
+                className="absolute sm:top-24 top-20 mt-1 sm:mt-0 sm:left-4 left-2 flex items-center gap-2 bg-[#4510a3] h-11 px-4 text-white rounded-md"
             >
                 <CaretCircleLeft size={23} weight="fill" />
                 <p>Página de Busca</p>
             </button>
 
-            <h1 className="text-2xl text-black font-bold mt-4">RESULTADO</h1>
+            <h1 className="sm:text-2xl text-lg text-black font-bold lg:mt-4 mt-24">RESULTADO</h1>
             <div className="flex flex-row justify-center gap-2 items-center mt-3">
-                <h2 className="text-xl text-black">O Documento se classifica como: </h2>
-                <p className="text-xl text-[#4510a3] font-semibold">{classificacao}</p>
+                <h2 className="sm:text-xl text-sm text-black">O Documento se classifica como: </h2>
+                <p className="sm:text-xl text-md text-[#4510a3] font-semibold">{classificacao}</p>
             </div>
 
-            <div className="flex gap-6 w-full justify-center">
-                <div className="bg-[#F9F9F9] hover:bg-[#efeef4] flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md w-1/3 gap-4 mt-8">
-                    <p className="text-xl">{`Nome do Arquivo: ${result.nomeArquivo}`}</p>
-                    <p className="text-xl">{`CNPJs: ${result.contrato.cnpjs.join(', ')}`}</p>
+            <div className="flex xl:gap-6 w-full xl:flex-row flex-col justify-center items-center">
+                <div className="bg-[#F9F9F9] w-full hover:bg-[#efeef4] flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md w-1/3 gap-4 sm:mt-8 mt-6">
+                    <p className="sm:text-xl text-md">{`Nome do Arquivo: ${result.nomeArquivo}`}</p>
+                    <p className="sm:text-xl text-md">{`CNPJs: ${result.contrato.cnpjs.join(', ')}`}</p>
                 </div>
-                <div className="bg-[#F9F9F9] hover:bg-[#efeef4] flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md w-1/3 gap-4 mt-8">
-                    <p className="text-xl">{`Contratante: ${result.contrato.contractant}`}</p>
-                    <p className="text-xl">{`Contratado: ${result.contrato.contractor}`}</p>
+                <div className="bg-[#F9F9F9] w-full hover:bg-[#efeef4] flex flex-col border-2 border-gray-300 p-3 py-5 rounded-md w-1/3 gap-4 sm:mt-8 mt-6">
+                    <p className="sm:text-xl text-md">{`Contratante: ${result.contrato.contractant}`}</p>
+                    <p className="sm:text-xl text-md">{`Contratado: ${result.contrato.contractor}`}</p>
                 </div>
             </div>
-            <p className="text-xl mt-8">{`Data de vigência do contrato: ${result.contrato.date}`}</p>
+            <p className="sm:text-xl text-md mt-8">{`Data de vigência do contrato: ${result.contrato.date}`}</p>
 
             {/*<iframe
                 src={pdfUrl}
